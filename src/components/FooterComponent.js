c579fee2d1987a6d4b7d80ad2bb903af93182339
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2022-01-11 00:41:00 +0800

    added birdy into projects and edited te footer to include IG

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -9,9 +9,11 @@ function Footer() {
         <div id="section-find-me">
             <IconContext.Provider value={{ color: '#f1f1f1ff' }}>
                 <div className="section-footer">
+                    <span>Find me at </span>
                     <a href="https://www.linkedin.com/in/mjerricho/" target="_blank" rel="noopener noreferrer"><AiIcons.AiFillLinkedin size={25}/></a>
                     <a href="https://github.com/mjerricho" target="_blank" rel="noopener noreferrer"><AiIcons.AiFillGithub size={25}/></a>
                     <a href="https://drive.google.com/drive/folders/1x_kigcNhVrcQp4sR8zIbHwJaQyKoiwn6?usp=sharing" target="_blank" rel="noopener noreferrer"><FaIcons.FaGoogleDrive size={25}/></a>
+                    <span><AiIcons.AiFillInstagram size={28}/>: @jerpret</span>
                     <span><AiIcons.AiFillMail size={25}/>: Marcellinus.jerricho@gmail.com</span>
                     <a href={resume} download>Resume</a>
                 </div>
--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -2,10 +2,19 @@ import projectautoware from '../assets/images/projects/projects-autoware.png';
 import projectwater from '../assets/images/projects/projects-water.png';
 import projectpolitics from '../assets/images/projects/projects-politics.jpg';
 import projectpl from "../assets/images/projects/projects-pl.png";
+import projectbirdy from "../assets/images/projects/projects-birdy.png";
 
 export const ProjectsData =[
     {
         id: 1,
+        title: "Birdy: A text Messaging app",
+        subtitle: "An implementation of a text messaging app using ReactJS and Firebase.",
+        description: "I implemented a text messaging app that used no SQL and hosted on Firebase. I used reactJS to build the application's front-end and Firebase JavaScript v9.6.2 to handle its back end. I utilised Firestore database and its API to enable a realtime update of the app. It enables users to login using their Google account. Features include a chat room creation and search capability.",
+        picture: projectbirdy,
+        link: "https://birdy-de2d0.web.app"
+    },
+    {
+        id: 2,
         title: "Investigation on Perceptual Learning",
         subtitle: "An investigation into how training improves our ability in a specific task.",
         description: "I provide JavaScript technical assistance and maintenance for the variosu online experiemnt tools. Moreover, I also implemented an offline Perceptual Learning experiment tool using PsychoPy and Pandas in Python as a back up for the online tool. This initiative increased the resilience of the study, especially given that we are using expensive MRI Scanner on a very limited number of experiementees under a very tight working schedule.",
@@ -13,7 +22,7 @@ export const ProjectsData =[
         link: "https://github.com/mjerricho/curate-pl-offline"
     },
     {
-        id: 2,
+        id: 3,
         title: "Analysing the dataflow of Autoware",
         subtitle: "An exploration into the dataflow and determinism of Autoware, an open-source autonomous driving technology",
         description: "I investigated how Autoware, an “All-in-One” open-source software for autonomous driving technology, utilises Robot Operating System (ROS), a flexible framework for writing robot software, to manage the dataflow within its program. Autoware is built on ROS, where there are nodes that communicate with each other either periodically (time-triggered) or sequentially (data-driven). My role is to look deep into the source code to determine the methods at which the nodes communicate and the functions they perform. From these efforts to better understand the particularities of robotics frameworks, we hope to develop tools that will help to optimise robotics software in the future.",
@@ -21,7 +30,7 @@ export const ProjectsData =[
         link: "https://github.com/SamKouteili/ROSModelling.git"
     },
     {
-        id: 3,
+        id: 4,
         title: "Research on Ethnic Politics in Southeast Asia",
         subtitle: "An exploration into the impact of ethnicity on politics in Indonesia by applying Natural Language Processing on politicians' speeches.",
         description: "My role was to obtain and verify data on major elections results in Indonesia to be used in R. I explored Indonesian government and news websites to look into politicians' details from different areas of the country. I applied text mining to debates data to generate word cloud and statistical analysis. I used Topic Modelling using Natural Language Processing packages in R to analyse the relationship between the semantics and the lexicon of the terms the politicians used. I also use term frequency–inverse document frequency (tf-idf) to look at the relevant words that the politicians use. On top of that, I analysed the speech in word bigrams to observe the frequency and relationship of consecutive words. I also performed data scraping to get more information about the politicians. The scripts I wrote enabled analysis that would otherwise be extremely difficult with manual analysis. As I am still working on this project, the Professor in charge of this project prohibit me from sharing the work.",
@@ -29,7 +38,7 @@ export const ProjectsData =[
         link: "https://github.com/mjerricho/research-ethnic-politics-sea"
     },
     {
-        id: 4,
+        id: 5,
         title: "Machine-Learning Investigation of the Geometry of Molecular Arrangement in Liquid Water",
         subtitle: "An exploration into the possible substates of liquid water through the application of unsupervised learning on the geometry of its molecular arrangement",
         description: "My role was to produce a Python Machine Learning code (unsupervised learning) that would identify clusters of states of water based on their molecular properties. I employed Scikit-Learn, Pandas, Seaborn and Matplotlib library to process and visualise data into clusters. We found intriguing evidence that there might be 2 separate sub-states in the liquid state. The project was ended early due to the Professor's other commitments.",
